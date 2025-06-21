a213d697dd63cfb1c0149491538456f85a386979
Bejzak <ejzakblazej@gmail.com>
2025-06-21 19:36:30 +0200

    Descriptor binding manager

--- sources/descriptor_set/descriptor_binding_manager.cpp
+++ sources/descriptor_set/descriptor_binding_manager.cpp
@@ -8,8 +8,9 @@
 namespace {
 
 constexpr uint32_t UNIFORM_BINDING = 0;
-constexpr uint32_t STORAGE_BINDING = 1;
-constexpr uint32_t TEXTURE_BINDING = 2;
+constexpr uint32_t DYNAMIC_UNIFORM_BINDING = 1;
+constexpr uint32_t STORAGE_BINDING = 2;
+constexpr uint32_t TEXTURE_BINDING = 3;
 
 } //namespace
 
@@ -18,7 +19,7 @@ DescriptorBindingManager::DescriptorBindingManager(const DescriptorSet& descript
 }
 
 TextureHandle DescriptorBindingManager::storeTexture(const Texture& texture) {
-	size_t handle = _textures.size();
+	const size_t handle = _textures.size();
 	_textures.push_back(texture.getVkImageView());
 
 	const VkDescriptorImageInfo imageInfo = {
@@ -37,9 +38,30 @@ TextureHandle DescriptorBindingManager::storeTexture(const Texture& texture) {
 	};
 
 	vkUpdateDescriptorSets(_descriptorSet.getDescriptorPool().getLogicalDevice().getVkDevice(), 1, &write, 0, nullptr);
+
 	return static_cast<TextureHandle>(handle);
 }
 
 BufferHandle DescriptorBindingManager::storeBuffer(const Buffer& buffer) {
-	return BufferHandle::INVALID;
+	const size_t handle = _buffers.size();
+	_buffers.push_back(buffer.getVkBuffer());
+
+	const VkDescriptorBufferInfo bufferInfo = {
+		.buffer = buffer.getVkBuffer(),
+		.range = buffer.getSize()
+	};
+
+	VkWriteDescriptorSet write = {
+		.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
+		.dstSet = _descriptorSet.getVkDescriptorSet(),
+		.dstBinding = UNIFORM_BINDING,
+		.dstArrayElement = handle,
+		.descriptorCount = 1,
+		.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
+		.pBufferInfo = &bufferInfo
+	};
+
+	vkUpdateDescriptorSets(_descriptorSet.getDescriptorPool().getLogicalDevice().getVkDevice(), 1, &write, 0, nullptr);
+
+	return static_cast<BufferHandle>(handle);
 }
