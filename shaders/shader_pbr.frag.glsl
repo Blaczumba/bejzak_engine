e098b1a07ed77ecc0338b574bb5212cba3a54643
Bejzak <ejzakblazej@gmail.com>
2025-10-20 18:33:59 +0200

    Add NVidia PCSS

--- shaders/shader_pbr.frag.glsl
+++ shaders/shader_pbr.frag.glsl
@@ -32,7 +32,83 @@ const float PI = 3.14159265359;
 const vec3 lightColor = vec3(10.0);
 const float ao = 1.0;
 
-float calculateShadow() {
+// PCSS
+const int BLOCKER_SEARCH_SAMPLES = 16;
+const int PCF_SAMPLES = 25;
+const float LIGHT_SIZE = 0.5; // tweak per light
+const float MIN_FILTER_SIZE = 2.0 / 2048.0;
+
+const vec2 poissonDisk[25] = vec2[](
+    vec2(-0.942, -0.399), vec2(0.945, -0.768),
+    vec2(-0.094, -0.929), vec2(0.345,  0.294),
+    vec2(-0.915,  0.783), vec2(-0.878, -0.1),
+    vec2(0.767,  0.857),  vec2(0.353, -0.874),
+    vec2(0.558, -0.212),  vec2(-0.354, -0.116),
+    vec2(-0.669, -0.69),  vec2(-0.775,  0.463),
+    vec2(0.203, -0.604),  vec2(0.963,  0.527),
+    vec2(-0.423,  0.915), vec2(-0.639, -0.229),
+    vec2(-0.377, -0.857), vec2(0.591, -0.56),
+    vec2(0.759,  0.169),  vec2(-0.015,  0.221),
+    vec2(0.278,  0.748),  vec2(-0.83,   0.093),
+    vec2(-0.486,  0.561), vec2(0.713,  0.285),
+    vec2(-0.265, -0.364)
+);
+
+// float averageBlockerDepth(vec2 uv, float receiverDepth, float searchRadius)
+// {
+//     float blockerSum = 0.0;
+//     int blockerCount = 0;
+// 
+//     for (int i = 0; i < BLOCKER_SEARCH_SAMPLES; ++i) {
+//         float shadowDepth = texture(uGlobalTexturesShadow[nonuniformEXT(pushConstants.shadow)], uv + poissonDisk[i] * searchRadius).r;
+//         if (shadowDepth < receiverDepth) {
+//             blockerSum += shadowDepth;
+//             blockerCount++;
+//         }
+//     }
+// 
+//     if (blockerCount == 0)
+//         return -1.0; // no blockers
+// 
+//     return blockerSum / float(blockerCount);
+// }
+// 
+// float filterPcf(vec2 uv, float receiverDepth, float filterRadius)
+// {
+//     float sum = 0.0;
+//     for (int i = 0; i < PCF_SAMPLES; ++i) {
+//         float shadowDepth = texture(uGlobalTexturesShadow[nonuniformEXT(pushConstants.shadow)], uv + poissonDisk[i] * filterRadius).r;
+//         sum += float(receiverDepth <= shadowDepth);
+//     }
+//     return sum / float(PCF_SAMPLES);
+// }
+// 
+// float calculateShadowPcss() {
+//     vec3 lightFrag = lightFragPosition.xyz / lightFragPosition.w;
+//     if (lightFrag.z >= 1.0)
+//         return 1.0;
+// 
+//     float receiverDepth = lightFrag.z;
+// 
+//     vec2 uv = lightFrag.xy;
+// 
+//     float searchRadius = LIGHT_SIZE * (receiverDepth / lightFragPosition.w);
+//     float avgBlockerDepth = averageBlockerDepth(uv, receiverDepth, searchRadius);
+// 
+//     if (avgBlockerDepth < 0.0)
+//         return 1.0;
+// 
+//     float penumbraRatio = (receiverDepth - avgBlockerDepth) / avgBlockerDepth;
+//     float filterRadius = clamp(penumbraRatio * LIGHT_SIZE, MIN_FILTER_SIZE, 0.02);
+// 
+//     float shadow = filterPcf(uv, receiverDepth, filterRadius);
+// 
+//     return shadow;
+// }
+
+// PCSS
+
+float calculateShadowPcf() {
     vec3 lightFrag = lightFragPosition.xyz / lightFragPosition.w;
     if(lightFrag.z >= 1.0)
         return 1.0;
@@ -105,5 +181,5 @@ void main() {
     vec3 Lo = (kD * albedo / PI + specular) * radiance * NdotL; 
 
     vec3 ambient = vec3(0.1) * albedo * ao;
-    outColor = vec4(ambient + calculateShadow() * Lo, 1.0);
+    outColor = vec4(ambient + calculateShadowPcf() * Lo, 1.0);
 }
